ec56cd19df56c010e449153fe16c3c31097b7788
melfx <130380863+melfx@users.noreply.github.com>
2023-10-23 08:50:32 -0300

    Add files via upload
    prova3tri

---
+++ duda/frontEnd3trimLeoncio-main/principal.js
@@ -0,0 +1,10 @@
+//Começo do site
+
+Let = document.querySelector(".titulo")
+//Cria o titulo
+h1>Calculadora <h1>
+
+<p>Veja aqui a idade humana do seu cachorro</p>
+Let = document.querySelector(".titulo")
+
+
